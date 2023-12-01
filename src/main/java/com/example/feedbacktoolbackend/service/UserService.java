0049a25cf4fa445a2a0450eb6915eac215fe4deb
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-01 11:11:22 +0100

    Revamped UserService to work with the updated business and controller

--- src/main/java/com/example/feedbacktoolbackend/service/UserService.java
+++ src/main/java/com/example/feedbacktoolbackend/service/UserService.java
@@ -1,46 +1,62 @@
 package com.example.feedbacktoolbackend.service;
 
-import com.example.feedbacktoolbackend.data.Models.User;
-import com.example.feedbacktoolbackend.data.UserRepository;
-import com.example.feedbacktoolbackend.service.models.UserBusiness;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+        import com.example.feedbacktoolbackend.controller.dto.AuthenticationDTO;
+        import com.example.feedbacktoolbackend.controller.exception.InvalidInputException;
+        import com.example.feedbacktoolbackend.data.UserRepository;
+        import com.example.feedbacktoolbackend.data.Models.User;
+        import com.example.feedbacktoolbackend.enums.Role;
+        import com.example.feedbacktoolbackend.service.models.UserBusiness;
+        import jakarta.persistence.EntityExistsException;
+        import jakarta.transaction.Transactional;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class UserService {
-    private final UserRepository userRepository;
+    private final PasswordEncodingService passwordEncoderService;
+    private final UserRepository repository;
 
-    /**
-     * Constructor injection for UserService.
-     * @author Sven Molenaar
-     * @param userRepository The repository for user data.
-     */
     @Autowired
-    public UserService(UserRepository userRepository){
-        this.userRepository = userRepository;
+    public UserService(PasswordEncodingService passwordEncoderService, UserRepository repository) {
+        this.passwordEncoderService = passwordEncoderService;
+        this.repository = repository;
     }
 
-    /**
-     * Retrieves a user in the business model format by ID.
-     * @author Sven Molenaar
-     * @param id The ID of the user.
-     * @return The user in business model format.
-     */
-    public UserBusiness getUser(Long id){
-        return convertToBusinessModel(userRepository.findById(id));
+    public UserBusiness createUser(AuthenticationDTO authenticationDTO) {
+        UserBusiness userBusiness = new UserBusiness(authenticationDTO.firstName(), authenticationDTO.prefixes(), authenticationDTO.lastName(), authenticationDTO.email(), passwordEncoderService.encodePassword(authenticationDTO.password()), Role.valueOf(authenticationDTO.role()));
+        if (userBusiness.hasValidEmail()) {
+            if (repository.existsByEmail(userBusiness.getEmail())) {
+                throw new EntityExistsException("User already exists");
+            } else {
+                return convertToUserBusiness(repository.save(convertToUserEntity(userBusiness)));
+            }
+        } else {
+            throw new InvalidInputException(userBusiness.getEmail());
+        }
     }
 
-    /**
-     * Converts user data from database entity to a business model object.
-     * @author Sven Molenaar
-     * @param userData The optional user data retrieved from the database.
-     * @return The user data in business model format or null if not found.
-     */
-    private UserBusiness convertToBusinessModel(Optional<User> userData){
-        return userData.map(user -> new UserBusiness(user.id, user.firstName, user.lastName)).orElse(null);
+    private User convertToUserEntity(UserBusiness userBusiness) {
+        User user = new User();
+        user.setId(userBusiness.getId());
+        user.setFirstName(userBusiness.getFirstName());
+        user.setPrefixes(userBusiness.getPrefixes());
+        user.setLastName(userBusiness.getLastName());
+        user.setEmail(userBusiness.getEmail());
+        user.setPassword(userBusiness.getPassword());
+        user.setRole(userBusiness.getRole());
+        return user;
     }
-}
+
+    private UserBusiness convertToUserBusiness(User userEntity) {
+        return new UserBusiness(
+                userEntity.getId(),
+                userEntity.getFirstName(),
+                userEntity.getPrefixes(),
+                userEntity.getLastName(),
+                userEntity.getEmail(),
+                userEntity.getPassword(),
+                userEntity.getRole()
+        );
+    }
+  }
