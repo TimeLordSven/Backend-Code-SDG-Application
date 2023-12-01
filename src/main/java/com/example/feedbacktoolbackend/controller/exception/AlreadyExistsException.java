3f295ce6eb7ea43cf998204d09d4faf83757e0bb
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-01 11:07:17 +0100

    Added Exceptions and new SecurityConfig

---
+++ src/main/java/com/example/feedbacktoolbackend/controller/exception/AlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.example.feedbacktoolbackend.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String entityName) {
+        super(entityName + " already exists.");
+    }
+}
