ba40c6dbe481c21f14ff383e88b6e39bd1e6046f
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-01 11:08:00 +0100

    Added Spring security and added password related functions

--- src/main/java/com/example/feedbacktoolbackend/service/PasswordEncodingService.java
+++ src/main/java/com/example/feedbacktoolbackend/service/PasswordEncodingService.java
@@ -1,4 +1,22 @@
 package com.example.feedbacktoolbackend.service;
+import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class PasswordEncodingService {
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public PasswordEncodingService() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public String encodePassword(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    public boolean matchPasswords(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
 }
