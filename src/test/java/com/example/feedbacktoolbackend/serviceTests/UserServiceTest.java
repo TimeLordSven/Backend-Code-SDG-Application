ebc609ebc9c93a4be44b43e9853172228ee29d21
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-05 11:59:34 +0100

    WORKING TESTS!!!!!!

--- src/test/java/com/example/feedbacktoolbackend/serviceTests/UserServiceTest.java
+++ src/test/java/com/example/feedbacktoolbackend/serviceTests/UserServiceTest.java
@@ -1,24 +1,16 @@
 package com.example.feedbacktoolbackend.serviceTests;
 
 import com.example.feedbacktoolbackend.controller.dto.AuthenticationDTO;
-import com.example.feedbacktoolbackend.controller.exception.InvalidInputException;
-import com.example.feedbacktoolbackend.data.UserRepository;
-import com.example.feedbacktoolbackend.service.PasswordEncodingService;
+import com.example.feedbacktoolbackend.controller.exception.*;
 import com.example.feedbacktoolbackend.service.UserService;
 import com.example.feedbacktoolbackend.service.models.UserBusiness;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
 @SpringBootTest
 @Transactional
 public class UserServiceTest {
@@ -117,71 +109,4 @@ public class UserServiceTest {
         assertThrows(InvalidInputException.class, () -> userService.createUser(authenticationDTO));
     }
 }
-//@SpringBootTest
-//class UserServiceTest {
-//
-//    @Mock
-//    private PasswordEncodingService passwordEncoderService;
-//
-//    @Mock
-//    private UserRepository repository;
-//
-//    @InjectMocks
-//    private UserService userService;
-//
-//    @BeforeEach
-//    void setUp() {
-//    }
-//    @Test
-//    void testRegistrationValidEverything1() {
-//        when(passwordEncoderService.encodePassword(any())).thenReturn("encodedPassword");
-//        when(repository.existsByEmail(any())).thenReturn(false);
-//        AuthenticationDTO authenticationDTO = new AuthenticationDTO(
-//                "Hva.com",
-//                "Abraham",
-//                "Van",
-//                "VanHelsing@Hva.com",
-//                "Password123!",
-//                "Password123!",
-//                "STUDENT"
-//        );
-//
-//        // Use assertDoesNotThrow without specifying exception type
-//        assertDoesNotThrow(() -> userService.createUser(authenticationDTO));
-//    }
-//}
-//    @Test
-//    void testRegistrationValidEverything2() {
-//        when(passwordEncoderService.encodePassword(any())).thenReturn("encodedPassword");
-//        when(repository.existsByEmail(any())).thenReturn(false);
-//        AuthenticationDTO authenticationDTO = new AuthenticationDTO(
-//                "Hva.com",
-//                "Abraham",
-//                "Van",
-//                "VanHelsing@Hva.com",
-//                "Password123!",
-//                "Password123!",
-//                "STUDENT"
-//        );
-//        assertDoesNotThrow(InvalidInputException.class, () -> userService.createUser(authenticationDTO));
-//    }
-//    @Test
-//    void testRegistrationInvalidEmail2() {
-//        when(passwordEncoderService.encodePassword(any())).thenReturn("encodedPassword");
-//        when(repository.existsByEmail(any())).thenReturn(false);
-//        AuthenticationDTO authenticationDTO = new AuthenticationDTO(
-//                "Abraham",
-//                "Van",
-//                "Helsing",
-//                "VanHelsing@Hvadem",
-//                "Password123!",
-//                "Password123!",
-//                "STUDENT"
-//        );
-//        assertThrows(InvalidInputException.class, () -> userService.createUser(authenticationDTO));
-//    }
-
-//}
-
-
 
