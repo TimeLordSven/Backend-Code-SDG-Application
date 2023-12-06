359226e5ed4f8101e0d005d0f224324a7a51c277
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-06 21:55:35 +0100

    Added comments to the AlreadyExistsException

--- src/main/java/com/example/feedbacktoolbackend/controller/exception/AlreadyExistsException.java
+++ src/main/java/com/example/feedbacktoolbackend/controller/exception/AlreadyExistsException.java
@@ -3,8 +3,17 @@ package com.example.feedbacktoolbackend.controller.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when an entity already exists.
+ * Extends RuntimeException and sets the HTTP status to BAD_REQUEST.
+ * @author Sven Molenaar
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class AlreadyExistsException extends RuntimeException {
+    /**
+     * Constructs an AlreadyExistsException with the provided entity name.
+     * @param entityName Name of the entity that already exists.
+     */
     public AlreadyExistsException(String entityName) {
         super(entityName + " already exists.");
     }
