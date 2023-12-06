75b9edea1dd455d0b01f9e8c5bfc7adcc128253f
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-06 21:56:32 +0100

    Added comments to the UserController

--- src/main/java/com/example/feedbacktoolbackend/controller/UserController.java
+++ src/main/java/com/example/feedbacktoolbackend/controller/UserController.java
@@ -10,16 +10,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller handling user-related operations.
+ * Maps endpoints related to student user creation.
+ * @author Sven Molenaar
+ * Parameters (userService, requestBody)
+ * Returns ResponseEntity with HTTP status
+ */
 @RestController
 @RequestMapping("/users/students")
 public class UserController {
     private final UserService userService;
 
+    /**
+     * Constructor injection for UserService.
+     * @author Sven Molenaar
+     * @param userService Service handling user operations
+     */
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Endpoint to create a new student user.
+     * Receives an AuthenticationDTO from the request body.
+     * Calls the UserService to create a new user.
+     * @author Sven Molenaar
+     * @param requestBody AuthenticationDTO object containing user data
+     * Returns ResponseEntity with CREATED status
+     */
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody AuthenticationDTO requestBody) {
         userService.createUser(requestBody);
