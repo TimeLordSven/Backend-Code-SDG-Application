091ba6f4596757ccc6ce41d7240878a236d2271f
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-06 22:00:36 +0100

    Added Comments to UserControllerTests

--- src/test/java/com/example/feedbacktoolbackend/controllerTests/UserControllerTest.java
+++ src/test/java/com/example/feedbacktoolbackend/controllerTests/UserControllerTest.java
@@ -1,21 +1,29 @@
 
 package com.example.feedbacktoolbackend.controllerTests;
-        import org.junit.jupiter.api.Test;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-        import org.springframework.boot.test.context.SpringBootTest;
-        import org.springframework.http.MediaType;
-        import org.springframework.test.web.servlet.MockMvc;
-        import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-        import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+/**
+ * Tests for the UserController class, specifically focusing on user creation endpoints.
+ * @author Sven Molenaar
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
+    /**
+     * Test case to validate the creation of a user through the UserController.
+     * Sends a POST request to the endpoint and checks for a successful user creation.
+     * @author Sven Molenaar
+     * @throws Exception if any error occurs during the test
+     */
     @Test
     public void testCreateUserEndpoint() throws Exception {
         String requestJson = "{\n" +
