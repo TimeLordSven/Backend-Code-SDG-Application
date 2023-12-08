abe999ef67ba4ea1744b1122e9653d29aae6ed42
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-08 11:58:09 +0100

    Formatted UserControllerTest

--- src/test/java/com/example/feedbacktoolbackend/controllerTests/UserControllerTest.java
+++ src/test/java/com/example/feedbacktoolbackend/controllerTests/UserControllerTest.java
@@ -1,5 +1,6 @@
 
 package com.example.feedbacktoolbackend.controllerTests;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,8 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 /**
  * Tests for the UserController class, specifically focusing on user creation endpoints.
+ *
  * @author Sven Molenaar
  */
 @SpringBootTest
@@ -18,11 +21,13 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     /**
      * Test case to validate the creation of a user through the UserController.
      * Sends a POST request to the endpoint and checks for a successful user creation.
-     * @author Sven Molenaar
+     *
      * @throws Exception if any error occurs during the test
+     * @author Sven Molenaar
      */
     @Test
     public void testCreateUserEndpoint() throws Exception {
