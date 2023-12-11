b815da69d16b9edf53ada9bf2aedc028a0d8772c
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-11 13:46:16 +0100

    Created SessionBusiness

--- src/main/java/com/example/feedbacktoolbackend/service/models/SessionBusiness.java
+++ src/main/java/com/example/feedbacktoolbackend/service/models/SessionBusiness.java
@@ -1,4 +1,42 @@
 package com.example.feedbacktoolbackend.service.models;
 
+
+import java.util.Date;
+
 public class SessionBusiness {
+    private String sessionId;
+    private UserBusiness user;
+    private Date createdAt;
+
+
+    public SessionBusiness(String sessionId, UserBusiness user, Date createdAt) {
+        this.sessionId = sessionId;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public UserBusiness getUser() {
+        return user;
+    }
+
+    public void setUser(UserBusiness user) {
+        this.user = user;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
