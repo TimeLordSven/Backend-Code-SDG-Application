cabeacea2d4dfefd982180ccc824c9846944058b
Sven Molenaar <sven.molenaar@hva.nl>
2023-12-22 11:28:17 +0100

    Added First stage of Middleware Exception Handling

--- src/main/java/com/example/feedbacktoolbackend/controller/dto/LoginDTO.java
+++ src/main/java/com/example/feedbacktoolbackend/controller/dto/LoginDTO.java
@@ -1,9 +1,17 @@
 package com.example.feedbacktoolbackend.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+
 /**
  * DTO (Data Transfer Object) representing login credentials.
+ *
  * @author Sven Molenaar
  */
-public record LoginDTO(String email, String password) {
-
-
+public record LoginDTO(
+        @Email
+        @NotBlank
+                (message = "Invalid email format") String email,
+        @NotBlank(message = "Password cannot be blank") String password
+) {
 }
--- src/main/java/com/example/feedbacktoolbackend/controller/dto/RegistrationDTO.java
+++ src/main/java/com/example/feedbacktoolbackend/controller/dto/RegistrationDTO.java
@@ -1,12 +1,42 @@
 package com.example.feedbacktoolbackend.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * Represents a Data Transfer Object (DTO) for user information.
  * This record encapsulates user data, providing a concise and immutable representation.
  *
  * @author Sven Molenaar
  */
-public record RegistrationDTO(String firstName, String prefixes, String lastName, String email, String password,
-                              String verifyPassword) {
+public record RegistrationDTO(
+        @NotBlank(message = "First name cannot be null or an empty string")
+        @Size(min = 1, max = 50, message = "First name should be between 1 and 50 characters long")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "First name can only contain alphabetic characters and cannot contain spaces")
+        String firstName,
+
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Prefix can only contain alphabetic characters")
+        String prefixes,
+
+        @NotBlank(message = "Last name cannot be null or an empty string")
+        @Size(min = 1, max = 50, message = "Last name should be between 1 and 50 characters long")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Last name can only contain alphabetic characters and cannot contain spaces")
+        String lastName,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+                message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, message = "Password should have at least 8 characters")
+        @Pattern(regexp = "^(?=.*[!@#$%^&*()_+{}|:\"<>?,./;'\\[\\]`~]).*$", message = "Password should contain at least one special character")
+        String password,
+
+        @NotBlank(message = "Verify password cannot be blank")
+        @Pattern(regexp = "^(?=.*[!@#$%^&*()_+{}|:\"<>?,./;'\\[\\]`~]).*$", message = "Verify password should contain at least one special character")
+        String verifyPassword
+) {
 
 }
