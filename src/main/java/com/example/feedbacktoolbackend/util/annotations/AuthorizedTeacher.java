30eedba584199527b735c4fd4aa1a7a41169a808
Sven Molenaar <sven.molenaar@hva.nl>
2024-01-09 11:40:28 +0100

    Created AuthorizedStudent and AuthorizedTeacher respecitively

---
+++ src/main/java/com/example/feedbacktoolbackend/util/annotations/AuthorizedTeacher.java
@@ -0,0 +1,20 @@
+package com.example.feedbacktoolbackend.util.annotations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TeacherRoleValidator.class)
+public @interface AuthorizedTeacher {
+    String message() default "No access rights";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
